6d5b4c2b21f5c0f9e6eb32a0fdcd9c91c62a4e19
= <anamika@nagarro.com>
2020-04-20 20:44:19 +0530

    assignment 1 completed

--- src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-assignment1',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment1.component.css']
 })
 export class Assignment1Component {
+ //feilds
+  @ViewChild('inputText') text:ElementRef; 
+  value:String;
+
+//method
+onKeydown(event:any):void{
+  this.value = this.text.nativeElement.value;
+}
 
 }
